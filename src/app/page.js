1d86b4f6ed39acb7f2d062d64fbf6aa03cf870d0
Riyanghouri786 <riyanghouri66@gmail.com>
2025-06-24 08:10:39 -0700

    set

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,11 @@
 import React from 'react'
 
-const Page = () => {
+const page = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>This is the home page of our application.</p>
-      <p>Here you can find various resources and information.</p>
-      <p>Feel free to explore the different sections.</p>
-      <p>We hope you enjoy your visit!</p>    
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main content of the home page.</p>
-      <p>Feel free to explore the site!</p>
-      <p>Enjoy your stay!</p>
-      <p>Have a great day!</p>
-      <p>Thank you for visiting!</p>
-      <p>We hope you find what you're looking for.</p>
-      <p>If you have any questions, feel free to reach out.</p>
-      <p>Stay tuned for more updates!</p>
-      <p>Check out our latest features!</p>
-      <p>Dont forget to subscribe to our newsletter!</p>
-      <p>Follow us on social media for more content!</p>  
+      <p>Riyan Ghouri</p>
     </div>
   )
 }
 
-export default Page
+export default page
